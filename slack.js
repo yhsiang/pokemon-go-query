cd41e9e2efb4db80e5215c437fa431444d8cfac5
LY Cheng <lyforever62@hotmail.com>
2016-09-09 15:38:24 +0800

    Simple daemon mode for slack

--- slack.js
+++ slack.js
@@ -17,10 +17,10 @@ function notify(text) {
 
 const latitude = process.env.LAT || 25.032101;
 const longitude = process.env.LNT || 121.5556329;
-const distance = process.env.DIST || 1000;
+const distance = process.env.DIST || 1200;
 
 function toText({ lat, long, pokemon, id, remain, dist }) {
-  const imageURL = `https://maps.googleapis.com/maps/api/staticmap?size=764x400&sensor=false&center=${latitude},${longitude}&zoom=16&markers=${lat},${long}&maptype=roadmap.jpg`;
+  const imageURL = `https://maps.googleapis.com/maps/api/staticmap?size=640x400&sensor=false&center=${latitude},${longitude}&markers=${lat},${long}&maptype=roadmap.jpg`;
   return `${pokemon}的位置，距離 ${dist} 公尺，剩下時間 ${remain}\n<${imageURL}>`;
 }
 
@@ -32,10 +32,13 @@ const ids = [
   141, 143, 149
 ];
 
-query({ latitude, longitude }, distance, pokemons => {
-  const filtered = pokemons.filter(({ id }) => ids.indexOf(id) > -1)
-  if (filtered.length > 0) {
-    let text = filtered.map(toText).join('\n');
-    notify(text);
-  }
-})
+const execute = () =>
+  query({ latitude, longitude }, distance, pokemons => {
+    const filtered = pokemons.filter(({ id }) => ids.indexOf(id) > -1)
+    if (filtered.length > 0) {
+      filtered.map(toText).map(notify);
+    }
+    setTimeout(() => execute(), 5 * 60 * 1000);
+  })
+
+execute();
